712b682abb89d8007cce7feb7619322fd7f5e51f
AreeshaJat <areesha.jat2715@gmail.com>
2024-06-10 17:02:24 -0400

    starter for input

--- src/create_todo.js
+++ src/create_todo.js
@@ -1,5 +1,6 @@
+const todo = [];
 class createTodo {
-    constructor(title, description, dueDate, priority, checklist = []) {
+    constructor(title, description, dueDate, priority, checklist) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -8,4 +9,34 @@ class createTodo {
     }
 }
 
-export default createTodo;
+function addTodoToList(title, description, dueDate, priority, checklist) {
+    let todoList = new createTodo(title, description, dueDate, priority, checklist);
+    todo.push(todoList);
+}
+
+function displayTodo() {
+    var todoCard = document.querySelector('.content');
+
+    //clear existing content
+    todoCard.innerHTML = "";
+
+    for (let i = 0; i < todo.length; i++) {
+        let todos = todo[i];
+
+        var card = document.createElement("div");
+        card.className = "card";
+
+        var priority = document.createElement("div");
+        priority.classList.add('priority');
+        priority.textContent = todo.priority;
+        card.appendChild(priority)
+
+        var checklist = document.createElement("button");
+        checklist.className = "checklist-toggle";
+        checklist.textContent = todos.checklist ? "Yes" : "No";
+
+        
+    }
+}
+
+export {createTodo};
